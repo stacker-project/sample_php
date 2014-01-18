e46f9cb0363a51803f76f6741c0357c6be6c9f25
Nick Willever <nickwillever@gmail.com>
2014-01-18 15:08:20 -0800

    merge fail fix

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
       Congrats! We are up and running!<br><br>  The pages below will let you know more information about the php environment
     </p>
     <ul>
-      <li><a href= "php_info.php">PHP Info</a> - Get more information on your php environment</li>      <li><a href= "database_test.php">PHP Database Test</a> - Make sure your php application can connect to the database</li>
+      <li><a href= "php_info.php">PHP Info</a> - Get more information on your php environment</li>
+      <li><a href= "database_test.php">PHP Database Test</a> - Make sure your php application can connect to the database</li>
    </ul>
   </body>
 </html>
